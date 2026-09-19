5cae529468ceaa5502d75f0b83b570d563a23790
Daniel Rodriguez <daniel.rodriguez6421@gmail.com>
2026-09-18 23:14:32 -0700

    Changed CMake.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
-#include <conduit/tests/InputTester.hpp>
-
 int main()
 {
-    conduit::InputTester inputTester;
-    inputTester.test();
-    
     std::cout << "Hello World!\n";
     return 0;
 }
